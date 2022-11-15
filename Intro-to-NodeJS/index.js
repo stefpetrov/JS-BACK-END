8a97a0292bcf52c528ec6c664228c214d6dadcbb
Stefan Petrov <stefspetrov@gmail.com>
2022-11-15 16:28:17 +0200

    switch url

--- Intro-to-NodeJS/index.js
+++ Intro-to-NodeJS/index.js
@@ -1,9 +1,29 @@
 const http = require("http");
 
 let app = http.createServer((req, res) => {
-    res.write('Hello Node.js')
+
+
+    switch (req.url) {
+        case '/':
+            res.write('<h1>HomePage</h1> <a href="/cats">Cats</a>')
+            break;
+        case '/cats':
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            res.write('<h2>CatsPage</h2>')
+            break;
+
+        default:
+            res.write('Error Page 404')
+            break;
+    }
+
     res.end()
 
 })
 
 app.listen(5000)
+
+
+console.log("Server is running on port 5000...")
