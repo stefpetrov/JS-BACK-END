03d72a2ad409b129ee067cf766a68006e64d8f40
Stefan Petrov <stefspetrov@gmail.com>
2022-11-25 11:50:45 +0200

    express JS demo

--- express-js-demo/index.js
+++ express-js-demo/index.js
@@ -3,9 +3,14 @@ const express = require('express')
 
 const app = express()
 
+
 app.get('/', (req, res) => {
 
-    res.write('Hello world from Express')
+    res.header({
+        'Content-Type': 'text/plain'
+    })
+    res.status(200)
+    res.write('<h1>Hello world from Express</h1>')
     res.end()
 })
 app.listen(5000, () => console.log('Server is running on port 5000...'))
