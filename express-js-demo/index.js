a2602758b8782dedd3dafd66fd42f8159c7f9097
Stefan Petrov <stefspetrov@gmail.com>
2022-11-25 15:00:57 +0200

    static files with app.use

--- express-js-demo/index.js
+++ express-js-demo/index.js
@@ -3,22 +3,30 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
-
-    res.header({
-        'Content-Type': 'text/html'
+    res.sendFile('./views/home/index.html', {
+        root: __dirname
     })
-    res.status(200)
-    res.write(`
-    <h1>Home</h1>
-    <ul>
-        <li><a href="/addBreed">Add Breed</a></li>
-        <li><a href="/addCat">Add Cat</a></li>
-    </ul>
-    `)
-    res.end()
 })
+
+
+// app.get('/', (req, res) => {
+
+//     res.header({
+//         'Content-Type': 'text/html'
+//     })
+//     res.status(200) 
+//     res.write(`
+//     <h1>Home</h1>
+//     <ul>
+//         <li><a href="/addBreed">Add Breed</a></li>
+//         <li><a href="/addCat">Add Cat</a></li>
+//     </ul>
+//     `)
+//     res.end()
+// })
 
 
 app.get('/addBreed', (req, res) => {
