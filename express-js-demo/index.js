91e12111f4b4ae47e0e5b81dfbe3b8bb464b23f4
Stefan Petrov <stefspetrov@gmail.com>
2022-11-25 13:26:57 +0200

    practice with Express router

--- express-js-demo/index.js
+++ express-js-demo/index.js
@@ -7,10 +7,67 @@ const app = express()
 app.get('/', (req, res) => {
 
     res.header({
-        'Content-Type': 'text/plain'
+        'Content-Type': 'text/html'
     })
     res.status(200)
-    res.write('<h1>Hello world from Express</h1>')
+    res.write(`
+    <h1>Home</h1>
+    <ul>
+        <li><a href="/addBreed">Add Breed</a></li>
+        <li><a href="/addCat">Add Cat</a></li>
+    </ul>
+    `)
     res.end()
 })
+
+
+app.get('/addBreed', (req, res) => {
+    res.header({
+        'Content-Type': 'text/html'
+    })
+    res.status(200)
+    res.write('<h1>Add Breed</h1>')
+    res.end()
+})
+app.get('/addCat', (req, res) => {
+    res.header({
+        'Content-Type': 'text/html'
+    })
+    res.status(200)
+    res.write('<h1>Add Cat</h1>')
+    res.end()
+})
+app.get('/cat/:catname', (req, res) => {
+    res.header({
+        'Content-Type': 'text/html'
+    })
+    res.status(200)
+    res.write(`
+    <h1>Load Cat Profile</h1>
+    <h2>${req.params.catname}</h2>
+    `)
+    res.end()
+})
+
+app.get('/add*', (req, res) => {
+    res.header({
+        'Content-Type': 'text/html'
+    })
+    res.status(200)
+    res.write('<h1>Add Something</h1>')
+    res.end()
+})
+//match with regex
+app.get(/.*cat.*/, (req, res) => {
+    res.header({
+        'Content-Type': 'text/html'
+    })
+    res.status(200)
+    res.write('<h1>Add cat with Regex match</h1>')
+    res.end()
+})
+
+
+
+
 app.listen(5000, () => console.log('Server is running on port 5000...'))
