e55abd8914138e501fd5495936f50e78f5f827e9
Stefan Petrov <stefspetrov@gmail.com>
2022-12-06 15:23:36 +0200

    add static hosting

--- cubicle-express-app/src/index.js
+++ cubicle-express-app/src/index.js
@@ -1,9 +1,12 @@
 const express = require("express")
+const path = require('path')
 
 const initHandlebars = require("./config/handlebars")
 const app = express()
 
 initHandlebars(app)
+
+app.use(express.static(path.resolve(__dirname, "./public")))
 
 app.all('/', (req, res) => {
     res.render("index")
