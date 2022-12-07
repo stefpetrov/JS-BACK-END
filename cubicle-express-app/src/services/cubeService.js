5a4eea16cbe4318082380146eb8779c1b3d6b4df
Stefan Petrov <stefspetrov@gmail.com>
2022-12-07 16:03:50 +0200

    add search by difficulty

--- cubicle-express-app/src/services/cubeService.js
+++ cubicle-express-app/src/services/cubeService.js
@@ -12,7 +12,20 @@ const create = (name, description, imageUrl, difficulty) => {
 
     Cube.add(cube)
 }
-const search = (text, from, to) => Cube.cubes.filter(x => x.name.toLowerCase().includes(text.toLowerCase()))
+const search = (text, from, to) => {
+    let result = Cube.cubes
+    if (text) {
+        result = result.filter(x => x.name.toLowerCase().includes(text.toLowerCase()))
+    }
+    if (from) {
+        result = result.filter(x => x.difficulty >= from)
+    }
+    if (to) {
+        result = result.filter(x => x.difficulty <= to)
+    }
+    return result
+}
+
 
 
 
