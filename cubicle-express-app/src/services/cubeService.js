3518da13d2cea7cb8fa179798433593d7062d6cd
Stefan Petrov <stefspetrov@gmail.com>
2022-12-07 14:24:37 +0200

    add details page

--- cubicle-express-app/src/services/cubeService.js
+++ cubicle-express-app/src/services/cubeService.js
@@ -3,15 +3,21 @@ const Cube = require('../models/Cube')
 
 const getAll = () => Cube.cubes
 
+const getOne = (id) => {
+   return Cube.cubes.find(x => x.id == id)
+}
+
 const create = (name, description, imageUrl, difficulty) => {
     let cube = new Cube(name, description, imageUrl, difficulty)
 
     Cube.add(cube)
 }
 
+
 const cubeService = {
     create,
-    getAll
+    getAll,
+    getOne
 }
 
 
