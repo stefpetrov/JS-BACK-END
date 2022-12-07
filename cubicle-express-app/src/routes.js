c1ffb670bad7f15bb367ec8baf6b368d8d58978a
Stefan Petrov <stefspetrov@gmail.com>
2022-12-07 14:02:10 +0200

    add error page

--- cubicle-express-app/src/routes.js
+++ cubicle-express-app/src/routes.js
@@ -6,5 +6,6 @@ const router = express.Router()
 
 router.use(homeController)
 router.use('/cube', cubeController)   // every URL starts with /cube is part of cubeController
+router.use('/*', (req, res) => res.render('404'))
 
 module.exports = router
