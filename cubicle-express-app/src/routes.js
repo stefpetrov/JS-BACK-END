fa523c9623cb9609c666af0cdb1f203b56f3bf5e
Stefan Petrov <stefspetrov@gmail.com>
2022-12-14 13:38:23 +0200

    add accessory functionalities

--- cubicle-express-app/src/routes.js
+++ cubicle-express-app/src/routes.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const cubeController = require('./controllers/cubeCotroller')
 const homeController = require('./controllers/homeCotroller')
+const accessoryController = require('./controllers/accessoryController')
+
 
 const router = express.Router()
 
 router.use(homeController)
 router.use('/cube', cubeController)   // every URL starts with /cube is part of cubeController
+router.use('/accessory', accessoryController)   
+
 router.use('/*', (req, res) => res.render('404'))
 
 
