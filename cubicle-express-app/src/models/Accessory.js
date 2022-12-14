1086ef9cbd810372ee14e0d55c7482a0aee62d64
Stefan Petrov <stefspetrov@gmail.com>
2022-12-14 13:53:28 +0200

    add accessory model

---
+++ cubicle-express-app/src/models/Accessory.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const AccessorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: [true, "Image Url is required!"],
+        validate: [/^https?:/i, "Image Url is invalid!"]
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 50
+    },
+})
+
+const Accessory = mongoose.model('Accessory', AccessorySchema)
+
+module.exports = Accessory
